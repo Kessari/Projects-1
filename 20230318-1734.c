37fc26a0fe8f54b8581623ee872ab801bda7e63e
Kessari <127944002+Kessari@users.noreply.github.com>
2023-03-18 17:40:50 +0100

    Add files via upload

---
+++ 20230318-1734.c
@@ -0,0 +1,16 @@
+void tri_decroissant(int tab[], int taille) {
+    int i, j, max, temp;
+
+    for(i=0; i<taille-1; i++) {
+        max = i;
+        for(j=i+1; j<taille; j++) {
+            if(tab[j] > tab[max]) {
+                max = j;
+            }
+        }
+        temp = tab[i];
+        tab[i] = tab[max];
+        tab[max] = temp;
+    }
+}
+    
